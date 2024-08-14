3593ed2c36de3c8ecfacef46ffd112bb76c2e8ec
Pratham <prathamjha18@gmail.com>
2024-08-14 22:12:18 +0530

    Hi

--- controller/imageController.js
+++ controller/imageController.js
@@ -6,7 +6,7 @@ export const uploadImage = async (request , response) => {
     }
 try {
        const file = await File.create(fileobj);
-       response.status(200).json({ path : `http://localhost:8000/file/${file._id}`})
+       response.status(200).json({ path : `https://filesharing-backend-wk2j.onrender.com/file/${file._id}`})
 } catch(error){
     console.error(error.message);
     response.status(500).json({ error : error.message })
